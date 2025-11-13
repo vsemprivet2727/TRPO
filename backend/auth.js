5dba20aa12c3688b6606a070609ac29ce580ecd7
Kovalskiy Ignat <dambosssss@gmail.com>
2025-11-13 22:03:27 +0000

    не знаю че писать ну кароче доделал окошечко аутентификации

--- backend/auth.js
+++ backend/auth.js
@@ -1,6 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     const authBox = document.querySelector('.auth-box');
     setTimeout(() => {
-        authBox.classList.add('auth-box.show');
+        authBox.classList.add('show');
     }, 100);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const togglePassword = document.getElementById('togglePassword');
+    const passwordInput = document.getElementById('password-input');
+    
+    if (togglePassword && passwordInput) {
+        togglePassword.addEventListener('click', function() {
+            const eyeIcon = this.querySelector('.eye-icon');
+            const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordInput.setAttribute('type', type);
+            
+            if (type === 'password') {
+                eyeIcon.src = 'resources/Eye_off.png';
+                eyeIcon.alt = 'Показать пароль';
+            } else {
+                eyeIcon.src = 'resources/Eye.png';
+                eyeIcon.alt = 'Скрыть пароль';
+            }
+        });
+    }
 });
