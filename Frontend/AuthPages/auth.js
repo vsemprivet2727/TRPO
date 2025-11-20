750e816a989251efd7a25da2f801bbc3dab23399
Kovalskiy Ignat <dambosssss@gmail.com>
2025-11-20 18:08:32 +0300

    merged auth.html and reg.html

--- Frontend/AuthPages/auth.js
+++ Frontend/AuthPages/auth.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    //Анимация появления блока
     const authBox = document.querySelector('.auth-box');
     setTimeout(() => {
         authBox.classList.add('show');
@@ -7,9 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     togglePassword('password-input', 'togglePassword')
     togglePassword('password-repeat-input', 'toggleRepeatPassword');
+    togglePassword('reg-password-input', 'toggleRegPassword');
+
+    linkClicked('loginForm', 'regForm', 'switchToReg');
+    linkClicked('regForm', 'loginForm', 'switchToLog');
 });
 
-//Функция переключения пароля
 function togglePassword(passwordID, iconID) {
     const togglePassword = document.getElementById(iconID);
     const passwordInput = document.getElementById(passwordID);
@@ -31,4 +33,60 @@ function togglePassword(passwordID, iconID) {
     }
 }
 
+function linkClicked(currentFormId, otherFormId, linkId) {
+    const currentForm = document.getElementById(currentFormId);
+    const otherForm = document.getElementById(otherFormId);
+    const link = document.getElementById(linkId);
+    if (otherForm && link) {
+        link.addEventListener('click', function(e){
+            e.preventDefault();
+            currentForm.style.display = 'none';
+            otherForm.style.display = 'block';
+        });
+    }
+}
+
+/*
+    <form action="" method="get">
+         <div class="flexscroll-box">
+             <div class="auth-box">
+                 <h2 align="center">Войдите в аккаунт</h2><br>
+                 <input type="text" id="login-text" placeholder="Логин">
+                 <div class="password-container">
+                     <input type="password" id="password-input" placeholder="Пароль" required>
+                     <span class="toggle-password" id="togglePassword">
+                     <img src="../resources/Eye.png" class="eye-icon" alt="Показать пароль">
+                     </span>
+                 </div>
+                 <button class="btn btn-primary"><b>Войти</b></button>
+                 <a href="Reg.html" id="goToReg">Нет учетной записи? Создать</a>
+                 </div>
+             </div>
+         </div>
+    </form>
+<form style="display:none;">
+        <div class="flexscroll-box">
+            <div class="auth-box">
+                <h2 align="center">Зарегистрируйтесь</h2><br>
+                <input type="text" id="login-text" placeholder="Логин">
+                <input type="email" id="email-input" placeholder="Эл. Почта" required>
+                <div class="password-container">
+                    <input type="password" id="password-input" placeholder="Пароль" required>
+                    <span class="toggle-password" id="togglePassword">
+                    <img src="../resources/Eye.png" class="eye-icon" alt="Показать пароль">
+                    </span>
+                </div>
+                <div class="password-container">
+                    <input type="password" id="password-repeat-input" placeholder="Повторите пароль" required>
+                    <span class="toggle-password" id="toggleRepeatPassword">
+                    <img src="../resources/Eye.png" class="eye-icon" alt="Показать пароль">
+                    </span>
+                </div>
+                <button class="btn btn-primary"><b>Создать</b></button>
+                <a href="Auth.html">Уже есть аккаунт? Войти</a>
+                </div>
+            </div>
+        </div>
+    </form>
+*/
 
